36a4c073a1391f45c491e034317670f282b4a6e4
Satyam Pawar <satyamspawar@gmail.com>
2023-01-16 18:12:05 +0530

    projects completed

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -5,9 +5,49 @@
 
 // ********** set date ************
 
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+
 // ********** close links ************
 
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  // linksContainer.classList.toggle("show-links");
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  // console.log(containerHeight);
+  const linksHeight = links.getBoundingClientRect().height;
+  // console.log(linksHeight);
+
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
+
 // ********** fixed navbar ************
+
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function () {
+  const scrollHeight = this.window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+
+  if (scrollHeight > 500) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
 
 // ********** smooth scroll ************
 // select links
